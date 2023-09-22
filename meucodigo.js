5334cdfa9d6566c2af2f6433373f2c322e49a717
DioneiCF <dionei.fagundes@outlook.com>
2023-09-22 14:51:33 -0300

    aula1.4

--- meucodigo.js
+++ meucodigo.js
@@ -5,5 +5,11 @@ var c = a + b;
 alert("Total da soma: " + c);
 
 var nome;
+
 nome = prompt("Informe o seu nome:");
 alert("Olá, " + nome);
+
+var idade;
+
+idade = prompt("Informe sua idade:");
+alert("Você tem, " + idade);
